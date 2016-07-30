3f457e76bb2b916b2de493582c1ab492c162e3d2
anjalik <anjalikmahajan@yahoo.com>
2016-07-30 11:29:12 -0700

    replaced hardcoded strings

--- app/src/main/java/com/codepath/todosmart/SplashActivity.java
+++ app/src/main/java/com/codepath/todosmart/SplashActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 public class SplashActivity extends AppCompatActivity {
 
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 5000;
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
